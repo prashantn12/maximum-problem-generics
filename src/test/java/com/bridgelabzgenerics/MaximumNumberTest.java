3137938f7065e8196735b57a03741d17733db1f1
prashantn/12 <prashantnangare0872@gmail.com>
2024-09-22 19:17:16 +0530

    Added: the maximum number is at the second position

--- src/test/java/com/bridgelabzgenerics/MaximumNumberTest.java
+++ src/test/java/com/bridgelabzgenerics/MaximumNumberTest.java
@@ -6,16 +6,16 @@ import static org.junit.Assert.assertEquals;
 public class MaximumNumberTest {
 
     @Test
-    public void givenMaxNumberAtFirstPosition_ShouldReturnSameNumber() {
+    public void givenMaxNumberAtSecondPosition_ShouldReturnSameNumber() {
         MaximumNumber<Integer> maximumNumber = new MaximumNumber<>();
 
-        // Test data with max number at the first position
-        Integer[] testData = {100, 20, 30};  
+        // Test data with max number at the second position
+        Integer[] testData = {20, 100, 30};  // 100 is the max number at second position
 
         // Invoke the method and get the result
         Integer result = maximumNumber.findMaximum(testData);
 
         // Assert that the result is the expected value (100)
-        assertEquals("Test case failed! Expected max at 1st position is 100", (Integer) 100, result);
+        assertEquals("Test case failed! Expected max at 2nd position is 100", (Integer) 100, result);
     }
 }
