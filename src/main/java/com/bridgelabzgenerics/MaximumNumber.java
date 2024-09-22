4a09d6efa28f45c102d89edd6e4bf4bfd000f50b
prashantn/12 <prashantnangare0872@gmail.com>
2024-09-22 18:04:11 +0530

    Added: Find Maximum of Three (or more) Integers using Generics and Array

--- src/main/java/com/bridgelabzgenerics/MaximumNumber.java
+++ src/main/java/com/bridgelabzgenerics/MaximumNumber.java
@@ -1,7 +1,31 @@
 package com.bridgelabzgenerics;
 
-public class MaximumNumber {
+public class MaximumNumber<T extends Comparable<T>> {
+
+    // Method to find maximum in an array of numbers
+    public T findMaximum(T[] elements) {
+        if (elements.length == 0) {
+            throw new IllegalArgumentException("Array is empty");
+        }
+
+        T max = elements[0];
+
+        for (T element : elements) {
+            if (element.compareTo(max) > 0) {
+                max = element;
+            }
+        }
+
+        return max;
+    }
+
     public static void main(String[] args) {
-        System.out.println("welcome to Generics Problems");
+        MaximumNumber<Integer> maximum = new MaximumNumber<>();
+
+        Integer[] array1 = {10, 20, 30};
+
+        System.out.println("Maximum of " + maximum.findMaximum(array1)); // 30
+
     }
 }
+
